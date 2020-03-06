73f82bf1d2a855c6c848a2edfab81bf91c351e99
randim <randim05@mail.ru>
2020-03-06 18:39:41 +0400

    2

--- test_errors.py
+++ test_errors.py
@@ -5,9 +5,31 @@
 # print(a)
 # print(help(a))
 
+
+# try:
+#     i = input("input something -->")
+# except:
+#     print("same error")
+# else:
+#     print("no error. you enther - {}".format(i))
+
+
+class myERROR(Exception):
+    def __init__(self, l, a):
+        Exception.__init__(self)
+        self.l = l
+        self.a = a
+
 try:
-    i = input("input something -->")
-except:
-    print("same error")
+    i = input("Input same ")
+    if len(i) < 3:
+        raise myERROR(len(i), 3)
+except EOFError:
+    print("IO error")
+except myERROR as ex:
+    print(('ShortInputException: The input was ' +
+           '{0} long, expected at least {1}')
+          .format(ex.l, ex.a))
 else:
-    print("no error. you enther - {}".format(i))
+    print("input correct")
+
