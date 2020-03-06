919e902a9729304113cb84620fcfe00f9c7f5cfb
randim <randim05@mail.ru>
2020-03-06 10:27:51 +0400

    2

--- robots.py
+++ robots.py
@@ -1,32 +1,32 @@
-class Robots():
+class Robots:
     population = 0
+
     def __init__(self, name):
         self.name = name
         print("Созданн {}".format(self.name))
         Robots.population += 1
 
-    def sayHi(self):
+    def say_hi(self):
         print("{} говорит привет!".format(self.name))
 
     def dyi(self):
         print("{} уничтожен".format(self.name))
         Robots.population -= 1
-
         if Robots.population == 0:
             print("Это был последний")
         else:
             print("Осталось {}".format(Robots.population))
 
     @classmethod
-    def howmach(cls):
+    def how_many(cls):
         print("Сейчас есть {:d} роботов".format(cls.population))
 
 
 r2 = Robots("R2-D2")
-r2.sayHi()
+r2.say_hi()
 c3 = Robots("C3PO")
-c3.sayHi()
+c3.say_hi()
 c3.dyi()
-r2.howmach()
+r2.how_many()
 r2.dyi()
-r2.howmach()
+Robots.how_many()
